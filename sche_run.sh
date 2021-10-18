faf84aca4ed468f4d09c1a51e7258895840cd8a2
geongupark <ggus.park@gmail.com>
2021-10-19 00:22:28 +0900

    20211019,

--- sche_run.sh
+++ sche_run.sh
@@ -19,4 +19,7 @@ args="$(date +%Y%m%d), ""${args}"
 
 # 3. Command for commit
 # > && : Run commands sequentially
-git add . && git commit -m "${args}" && git push && echo "geongupark" && echo "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"
+git add . && git commit -m "${args}" && git push <<EOF
+geongupark
+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
+EOF
