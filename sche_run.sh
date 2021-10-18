36f1188b3da5477af3ec4839d2c8b0451119137d
geongupark <ggus.park@gmail.com>
2021-10-18 23:11:38 +0900

    Delete venv_gus directory

--- sche_run.sh
+++ sche_run.sh
@@ -2,7 +2,7 @@
 
 # 0. Execute main pgm in venv
 CUR_PATH=$(pwd)
-source $CUR_PATH/venv_gus/bin/activate
+source $CUR_PATH/../venv_gus/bin/activate
 pwd
 python3 $CUR_PATH/Sise_trans_style.py
 deactivate
@@ -19,5 +19,5 @@ args="$(date +%Y%m%d), ""${args}"
 
 # 3. Command for commit
 # > && : Run commands sequentially
-git add . && git commit -m "${args}" && git push origin master
+git add . && git commit -m "${args}" && git push
 
